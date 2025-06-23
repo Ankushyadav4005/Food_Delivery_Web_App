5678137b04edcf66097519838b195d4819da9ce8
Ankush Yadav <ankushyadav4005@gmail.com>
2025-06-23 19:57:15 +0530

    new updated

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -7,7 +7,9 @@ const Header = () => {
     <div className='Header-contents'>
         <h2>Oder your favourite food here</h2>
         <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. one delicious meal at a time  </p>
-        <button>View Menu</button>
+        <button onClick={() => document.getElementById('explore-menu').scrollIntoView({ behavior: 'smooth' })}>
+  View Menu
+</button>
     </div>
     </div>
   )
